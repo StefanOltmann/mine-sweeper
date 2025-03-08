48124bd87df4ec48a639c4a34719a599810c0aaf
Stefan Oltmann <github@stefan-oltmann.de>
2025-03-08 12:36:41 +0100

    Right click implemented for WASM

--- app/src/wasmJsMain/kotlin/de/stefan_oltmann/mines/Platform.wasmJs.kt
+++ app/src/wasmJsMain/kotlin/de/stefan_oltmann/mines/Platform.wasmJs.kt
@@ -1,8 +1,46 @@
+/*
+ * 💣 Mines 💣
+ * Copyright (C) 2025 Stefan Oltmann
+ * https://github.com/StefanOltmann/mines
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package de.stefan_oltmann.mines
 
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.isSecondary
+import androidx.compose.ui.input.pointer.pointerInput
 
-actual fun Modifier.addRightClickListener(onClick: (Offset) -> Unit): Modifier {
-    return this
-}
+@OptIn(ExperimentalComposeUiApi::class)
+actual fun Modifier.addRightClickListener(onClick: (Offset) -> Unit): Modifier =
+    this.pointerInput(Unit) {
+        awaitPointerEventScope {
+            while (true) {
+
+                val event = awaitPointerEvent()
+
+                val change = event.changes.first()
+
+                if (change.pressed && event.button.isSecondary) {
+
+                    val offset = change.position
+
+                    onClick(offset)
+                }
+            }
+        }
+    }
